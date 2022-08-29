06247ad5a225f0bd81013bf1f46cf5db844a5630
Vadim <vadim.vasiliev.pro@gmail.com>
2022-08-29 10:04:46 +0300

    added validate of user birthday

--- line_story_drf/jwtauth/serializers.py
+++ line_story_drf/jwtauth/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib import auth
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.core.exceptions import ValidationError
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
+from django.utils.timezone import now
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.exceptions import TokenError
@@ -14,16 +16,29 @@ User = auth.get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True, label='Confirm password')
+    birthday = serializers.DateField(required=True, style={'input_type': 'date'})
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'password2']
+        fields = ['email', 'password', 'password2', 'birthday']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, attrs):
+        dob = attrs['birthday']
+        today = now()
+        age = today.year - dob.year - (
+                (today.month, today.day) < (dob.month, dob.day))
+        if age < 18:
+            raise ValidationError('Must be at least 18 years old to register')
+
+        data = super(RegisterSerializer, self).validate(attrs)
+        return data
 
     def create(self, validated_data):
         email = validated_data['email']
         password = validated_data['password']
         password2 = validated_data['password2']
+        birthday = validated_data['birthday']
 
         if email and User.objects.filter(email=email).exists():
             raise serializers.ValidationError(
@@ -32,7 +47,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         if password != password2:
             raise serializers.ValidationError({'password': 'The two passwords differ.'})
 
-        user = User(email=email)
+        user = User(email=email, birthday=birthday)
         user.set_password(password)
         user.is_active = False
         user.save()
