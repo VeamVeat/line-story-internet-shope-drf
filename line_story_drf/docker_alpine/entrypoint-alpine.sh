fe9d0e95fa024646083fef62881bebb56be2ef9a
Vadim <vadim.vasiliev.pro@gmail.com>
2022-10-05 12:57:21 +0300

    feat: added docker services alpine and Makefile

--- line_story_drf/docker_alpine/entrypoint-alpine.sh
+++ line_story_drf/docker_alpine/entrypoint-alpine.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 sleep 1
-python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 exec "$@"
--- line_story_drf/entrypoint.sh
+++ line_story_drf/entrypoint.sh
@@ -3,5 +3,4 @@
 sleep 1
 python manage.py flush --no-input
 python manage.py migrate --no-input
-python manage.py collectstatic --no-input --clear
 exec "$@"
